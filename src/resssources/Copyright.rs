46297ab4cb8c160dd555ec37da046317e9dd95b4
guileas <ethevegwenola@gmail.com>
2023-02-03 16:19:18 +0100

    feat: create stuct copyright, pattern

---
+++ src/resssources/Copyright.rs
@@ -0,0 +1,5 @@
+pub struct Copyright {
+    name: &str,
+    year: Option<((u8, u8), u8)>,
+    email: Option<&str>,
+}
--- src/resssources/FileParagraph.rs
+++ src/resssources/FileParagraph.rs
@@ -1,6 +1,12 @@
 pub struct FileParagraph {
     files: &str,
-    copyright: &str,
+    copyright: Vec<Copyright>,
     license: &str,
     comment: Option<&str>,
+}
+
+pub struct Pattern {
+    path: &str,
+    is_pattern: bool,
+    is_file: bool
 }
